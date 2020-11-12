74b4377dcad0196e8595fb7353adc069adc1b964
initzx <initzx>
2020-11-12 17:43:51 +0100

    - added some missing packages into buildrequires.json - made fetch_packages.py able to download packages - added new feature to parse.py that generates a spec file for all downloaded packages

--- parser/fetch_packages.py
+++ parser/fetch_packages.py
@@ -1,4 +1,5 @@
 import json
+import os
 import re
 import shutil
 from collections import deque
@@ -14,13 +15,8 @@ from pip._internal.network.session import PipSession
 from pip._vendor.packaging.specifiers import SpecifierSet
 
 SOURCE_URL = 'https://raw.githubusercontent.com/ansible/awx/devel/requirements/requirements.txt'
-
+PACKAGES_DIR = 'packages'
 pypi_session = requests.sessions.Session()
-
-
-def unpack_files(dest):
-    dest_dir = dest.split('/')[0]
-    shutil.unpack_archive(dest, dest_dir)
 
 
 def get_best_package(package_name, specifier=''):
@@ -43,14 +39,20 @@ def get_best_package(package_name, specifier=''):
     return cand.best_candidate.name, cand.best_candidate.version, cand.best_candidate.link.url
 
 
-def download_best_package(package_name, specifier=''):
+def download_best_package(package_name, specifier='', path=PACKAGES_DIR):
     name, version, url = get_best_package(package_name, specifier)
     url_split = url.split('#sha256')[0]
     file_split = url_split.split('/')[-1]
-    dest = str(name) + "/" + str(file_split)
+
+    if not os.path.exists(f'{path}/{name}'):
+       os.makedirs(f'{path}/{name}')
+
+    dest = f'{path}/{name}/{file_split}'
     r = requests.get(url_split, allow_redirects=True)
     open(dest, 'wb').write(r.content)
-    unpack_files(dest)
+    shutil.unpack_archive(dest, f'{path}/{package_name}')
+    extracted = dest.rstrip('.tar.gz')
+    os.rename(extracted, extracted.lower())
 
 
 def get_package_info(package):
@@ -167,11 +169,27 @@ def fetch_all_requirements_reqs():
     return condensed
 
 
-if __name__ == '__main__':
-    res = fetch_all_requirements_reqs()
+def download_all_packages(condensed=None):
+    if condensed:
+        with open(condensed, 'r') as fp:
+            condensed = json.load(fp)
+    else:
+        condensed = fetch_all_requirements_reqs()
 
-    with open('fetched_all.json', 'w') as fp:
-        json.dump(res, fp, indent=4)
+    if not os.path.exists(PACKAGES_DIR):
+        os.makedirs(PACKAGES_DIR)
+
+    for pkg in condensed.values():
+        print(f'Downloading {pkg["name"]}')
+        download_best_package(pkg['name'], specifier='=='+pkg['definite_version'])
+
+
+if __name__ == '__main__':
+    download_all_packages('out.json')
+    # res = fetch_all_requirements_reqs()
+
+    # with open('fetched_all.json', 'w') as fp:
+    #     json.dump(res, fp, indent=4)
 
     # facit = fetch_all_from_source()
     # with open('fetched_all.json', 'r') as fp:
